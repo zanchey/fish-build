4cf767e62b33b751d447cc4100b8d854d543d401
David Adam (zanchey) <zanchey@ucc.gu.uwa.edu.au>
2014-01-30 08:12:51 +0800

    build-fish-nightlies.zsh: update available Ubuntu releases

--- build-fish-nightlies.zsh
+++ build-fish-nightlies.zsh
@@ -17,7 +17,7 @@ DPKG_AREA=$BUILD_AREA/dpkgs
 OBS_AREA=$BUILD_AREA/obs
 
 # build for the following PPA architectures:
-PPA_SERIES=(precise quantal raring)
+PPA_SERIES=(precise quantal saucy trusty)
 # (zsh array - will not work in bash or sh)
 
 GPG_KEYID=0C273BBA
